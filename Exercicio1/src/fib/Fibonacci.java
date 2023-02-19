5aab37801b200d596351ad7def590ade7b06f405
Nycolas Kevin <nycolas.kevin2@gmail.com>
2023-02-19 10:38:10 -0300

    Exercicio 1 finalizado

--- Exercicio1/src/fib/Fibonacci.java
+++ Exercicio1/src/fib/Fibonacci.java
@@ -3,41 +3,57 @@ package fib;
 public class Fibonacci {
 
     public int fib1(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        return (fib1(n-1) + fib1(n-2));
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return (fib1(n - 1) + fib1(n - 2));
     }
 
-    public int fib2(int n) {
+    public void printFibWhile(int n) {
         int n1 = 0;
         int n2 = 1;
         int temp;
 
-        int count = 1;
-        while (count < n) {''
+        int count = 0;
+        while (count < n) {
+            System.out.print(n1 + " ");
             temp = n1 + n2;
             n1 = n2;
             n2 = temp;
             count++;
         }
-        return n1;
+        System.out.println();
     }
 
-    public int fib3(int n) {
+    public void printFibFor(int n) {
         int n1 = 0;
         int n2 = 1;
         int temp;
 
-        for (int count = 1; count < n; count++) {
+        for (int count = 0; count < n; count++) {
+            System.out.print(n1 + " ");
             temp = n1 + n2;
             n1 = n2;
             n2 = temp;
         }
-        return n1;
+        System.out.println();
     }
-    
+
     public static void main(String[] args) {
         Fibonacci f = new Fibonacci();
-        System.out.println(fib1(20));
+
+        System.out.println("Fibonacci com recursividade:");
+        for (int i = 0; i < 20; i++) {
+            System.out.print(f.fib1(i) + " ");
+        }
+        System.out.println();
+
+        System.out.println("Fibonacci com laço while:");
+        f.printFibWhile(20);
+
+        System.out.println("Fibonacci com laço for:");
+        f.printFibFor(20);
+
     }
 }
