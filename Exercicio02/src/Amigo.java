4bc4fb6f81dc5bef4b3f9dfcd267d4fa3202c276
NycolasKCN <nycolas.costa@dcx.ufpb.br>
2023-02-28 17:19:41 -0300

    Class amigo created

---
+++ Exercicio02/src/Amigo.java
@@ -0,0 +1,36 @@
+public class Amigo {
+    private String nome;
+    private String email;
+    private String emailAmigoSorteado;
+
+    public Amigo(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+        this.emailAmigoSorteado = null;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmailAmigoSorteado() {
+        return emailAmigoSorteado;
+    }
+
+    public void setAmigoSorteado(String emailAmigoSorteado) {
+        this.emailAmigoSorteado = emailAmigoSorteado;
+    }
+
+}
