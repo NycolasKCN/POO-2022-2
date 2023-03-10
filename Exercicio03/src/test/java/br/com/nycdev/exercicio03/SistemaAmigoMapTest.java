dd47012458c6d79ca4e1e97467b6057390c3f4f7
Nycolas Kevin <nycolas.kevin2@gmail.com>
2023-03-10 09:36:07 -0300

    Update SistemaAmigoMapTest.java

--- Exercicio03/src/test/java/br/com/nycdev/exercicio03/SistemaAmigoMapTest.java
+++ Exercicio03/src/test/java/br/com/nycdev/exercicio03/SistemaAmigoMapTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class SistemaAmigoTest {
+public class SistemaAmigoMapTest {
 
 	SistemaAmigoMap sistema;
 	
