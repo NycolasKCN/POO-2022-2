bb9e0282a7199742f8ac7955b34aff9dc7c79cf2
Nycolas Kevin Costa Nascimento <nycolas.costa@labs.dcx>
2023-03-23 11:42:43 -0300

    AgendaGUI class created

--- Aula23-03/src/main/java/br/com/nycdev/AgendaGUI.java
+++ Aula23-03/src/main/java/br/com/nycdev/AgendaGUI.java
@@ -6,10 +6,19 @@ import java.awt.*;
 public class AgendaGUI extends JFrame {
     AgendaGUI() {
         setTitle("Agenda de aniversários");
-        setSize(450,200);
+        setSize(450,450);
         setLocation(150,150);
         setResizable(false);
         getContentPane().setBackground(Color.lightGray);
+
+        setLayout(new GridLayout(3,1));
+        
+
+        JLabel text = new JLabel("Agenda de Aniversarios");
+
+        add(text);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args){
