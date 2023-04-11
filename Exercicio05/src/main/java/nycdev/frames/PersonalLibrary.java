7a9c7f87547b7945e4a73d03a0949ab71929e381
Nycolas Kevin <nycolas.kevin2@gmail.com>
2023-04-11 10:30:42 -0300

    PersonalLibrary with imageIcon

--- Exercicio05/src/main/java/nycdev/frames/PersonalLibrary.java
+++ Exercicio05/src/main/java/nycdev/frames/PersonalLibrary.java
@@ -9,42 +9,21 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class PersonalLibrary extends JFrame {
-
-    JLabel title, image;
-    JMenuBar menuBar = new JMenuBar();
+public class PersonalLibrary {
+    JFrame mainFrame;
     JFrame cadastraFrame;
+    JMenuBar menuBar = new JMenuBar();
+    JLabel title, image;
     ImageIcon reiImg = new ImageIcon("src/main/resources/assets/reiImg.png");
 
     SistemaLivro sistema;
 
-
     public PersonalLibrary() {
         sistema = new SistemaLivro();
-        JFrame f = this;
-        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                int resp = JOptionPane.showConfirmDialog(f, "Tem certeza de fechar?");
-                if (resp == JOptionPane.YES_OPTION) {
-                    System.exit(0);
-                }
-            }
-        });
-
-
-        setSize(800,600);
-        setLocation(300, 50);
-        setResizable(false);
-        setTitle("PersonalLibrary");
-        setBackground(Color.lightGray);
+        configFrame();
 
         cadastraFrame = new CadastrarLivroFrame(sistema);
 
-        title = new JLabel("Bem vindo, User", JLabel.CENTER);
-        title.setFont(new Font("serif", Font.BOLD, 28));
-        image = new JLabel(reiImg, JLabel.CENTER);
 
         JMenu cadastrarMenu = new JMenu("Cadastrar");
         JMenuItem cadastraLivro = new JMenuItem("Cadastar novo livro");
@@ -67,15 +46,49 @@ public class PersonalLibrary extends JFrame {
         menuBar.add(pesquisaMenu);
         menuBar.add(excluirMenu);
 
-        setJMenuBar(menuBar);
-        setLayout(new GridLayout(1,2));
-        add(title);
-        add(image);
 
     }
 
+    public void configLabels(){
+        title = new JLabel("Bem vindo, User", JLabel.CENTER);
+        title.setFont(new Font("serif", Font.BOLD, 28));
+        image = new JLabel(reiImg, JLabel.CENTER);
+
+    }
+
+    public void configFrame(){
+        mainFrame = new JFrame();
+        mainFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        mainFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int resp = JOptionPane.showConfirmDialog(mainFrame, "Tem certeza de fechar?");
+                if (resp == JOptionPane.YES_OPTION) {
+                    System.exit(0);
+                }
+            }
+        });
+
+        mainFrame.setBackground(Color.lightGray);
+        mainFrame.setLocation(300, 50);
+        mainFrame.setResizable(false);
+        mainFrame.setSize(800,600);
+        mainFrame.setTitle("PersonalLibrary");
+        mainFrame.setJMenuBar(menuBar);
+        mainFrame.setLayout(new GridLayout(1,2));
+        mainFrame.add(title);
+        mainFrame.add(image);
+    }
+
+    public void configCadastraFrame(){
+
+    }
+
+    public void run() {
+        mainFrame.setVisible(true);
+    }
     public static void main(String[] args) {
-        JFrame f = new PersonalLibrary();
-        f.setVisible(true);
+        PersonalLibrary o = new PersonalLibrary();
+        o.run();
     }
 }
